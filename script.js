a068b25238b644c0654ca0c5c039d261ef38853c
Callmeteddyfx <136664394+Callmeteddyfx@users.noreply.github.com>
2025-01-04 17:59:32 -0800

    Changes Made

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function getexpDate(){
+  const frontEnd = document.getElementById('frontendexp');
+  const frontendDate = new Date('2024-10-31');
+  const frontendMonth = frontendDate.getMonth();
+  const frontendYear = frontendDate.getFullYear();
+
+  //current Date
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth();
+  const currentYear = currentDate.getFullYear();
+  
+  //calculate Front End
+  const frontendMonthdif = (currentYear - frontendYear)* 12 + (currentMonth - frontendMonth);
+  frontEnd.textContent = frontendMonthdif + ' Months';
+}
+
+
+getexpDate();
+window.onload = getexpDate;
